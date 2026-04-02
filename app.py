f4b58547025f15e694d825ba4b5bfd48336ce644
Vladimir <122758630+N-Vladimire@users.noreply.github.com>
2026-04-02 15:36:40 +0000

    Nav bar fonctionnel

--- app.py
+++ app.py
@@ -9,6 +9,96 @@ def hello_world():
 
 # Déposez votre code à partir d'ici :
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.get("/paris")
+def api_paris():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    temps = data.get("hourly", {}).get("temperature_2m", [])
+
+    n = min(len(times), len(temps))
+    result = [
+        {"datetime": times[i], "temperature_c": temps[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphique.html")
+
+@app.route("/histogramme")
+def histogramme():
+    return render_template("histogramme.html")
+
+
+
+@app.get("/atelier-data")
+def atelier_data():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=43.2965&longitude=5.3698&hourly=windspeed_10m"
+    
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    values = data.get("hourly", {}).get("windspeed_10m", [])
+
+    n = min(len(times), len(values))
+
+    result = [
+        {"datetime": times[i], "value": values[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+@app.route("/atelier")
+def atelier():
+    return render_template("atelier.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
